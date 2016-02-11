76700a960a246ecbcb37dbfd8e0b645d9af77618
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-11 14:10:22 +0700

    Create Model, Views (_Layout) and Controller

---
+++ SportsStoreREST/App_Start/IdentityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStoreREST.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
