c88291410e783ab93521d6ab12080147d8c3c327
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-11 14:21:15 +0700

    Edit logic for Controller class

--- SportsStoreREST/Controllers/PrepController.cs
+++ SportsStoreREST/Controllers/PrepController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SportsStoreREST.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,38 @@ namespace SportsStoreREST.Controllers
 {
     public class PrepController : Controller
     {
-        // GET: Prep
+        IRepository repo;
+        public PrepController()
+        {
+            repo = new ProductRepository();
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(repo.Products);
+        }
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await repo.DeleteProductAsync(id);
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> SaveProduct(Product product)
+        {
+            await repo.SaveProductAsync(product);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Orders()
+        {
+            return View(repo.Orders);
+        }
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            await repo.DeleteOrderAsync(id);
+            return RedirectToAction("Orders");
+        }
+        public async Task<ActionResult> SaveOrder(Order order)
+        {
+            await repo.SaveOrderAsync(order);
+            return RedirectToAction("Orders");
         }
     }
 }
