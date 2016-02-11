ea6d7c7335f160a4a519092475102c9973d092c7
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-11 14:49:02 +0700

    Add Identity for User

---
+++ SportsStoreREST/Infrastructure/Identity/StoreRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStoreREST.Infrastructure.Identity
+{
+    public class StoreRole : IdentityRole
+    {
+        public StoreRole() : base() { }
+        public StoreRole(string name) : base(name) { }
+    }
+}
