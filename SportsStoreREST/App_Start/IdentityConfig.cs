ac1017188ef651688241c2c8396f4d43a712af6c
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-11 14:56:27 +0700

    Create view for Admin Role

--- SportsStoreREST/App_Start/IdentityConfig.cs
+++ SportsStoreREST/App_Start/IdentityConfig.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
+using SportsStoreREST.Infrastructure.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +15,16 @@ namespace SportsStoreREST.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
-
+            app.CreatePerOwinContext<StoreIdentityDbContext>(StoreIdentityDbContext.Create);
+            app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
+            app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+            app.UseCookieAuthentication
+                (
+                    new CookieAuthenticationOptions
+                    {
+                        AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+                    }
+                );
         }
     }
 }
--- SportsStoreREST/Controllers/PrepController.cs
+++ SportsStoreREST/Controllers/PrepController.cs
@@ -1,4 +1,8 @@
-﻿using SportsStoreREST.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SportsStoreREST.Infrastructure.Identity;
+using SportsStoreREST.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +23,13 @@ namespace SportsStoreREST.Controllers
         {
             return View(repo.Products);
         }
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> DeleteProduct(int id)
         {
             await repo.DeleteProductAsync(id);
             return RedirectToAction("Index");
         }
+        [Authorize(Roles = "Administrators")]
         public async Task<ActionResult> SaveProduct(Product product)
         {
             await repo.SaveProductAsync(product);
@@ -42,6 +48,20 @@ namespace SportsStoreREST.Controllers
         {
             await repo.SaveOrderAsync(order);
             return RedirectToAction("Orders");
+        }
+        public async Task<ActionResult> SignIn()
+        {
+            IAuthenticationManager authMgr = HttpContext.GetOwinContext().Authentication;
+            StoreUserManager userMrg = HttpContext.GetOwinContext().GetUserManager<StoreUserManager>();
+            StoreUser user = await userMrg.FindAsync("Admin", "secret");
+            authMgr.SignIn(await userMrg.CreateIdentityAsync(user,
+            DefaultAuthenticationTypes.ApplicationCookie));
+            return RedirectToAction("Index");
+        }
+        public ActionResult SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
