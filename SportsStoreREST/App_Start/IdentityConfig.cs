b901562a5178d388f38c3df0265d61ba318b2a5d
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-11 14:34:12 +0700

    Add Owin Assembly in identityConfig class

--- SportsStoreREST/App_Start/IdentityConfig.cs
+++ SportsStoreREST/App_Start/IdentityConfig.cs
@@ -1,9 +1,11 @@
-﻿using Owin;
+﻿using Microsoft.Owin;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+[assembly: OwinStartup(typeof(SportsStoreREST.App_Start.IdentityConfig))]
 namespace SportsStoreREST.App_Start
 {
     public class IdentityConfig
